f3d2addb51f0a2d7821cfb6716bf47a5ea42a78b
David Baker <dpbaker1298@gmail.com>
2023-11-11 11:28:02 -0500

    Add function to create temporary directory for testing

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -34,4 +37,24 @@ func TestRun(t *testing.T) {
 			}
 		})
 	}
+}
+
+func createTempDir(t *testing.T, files map[string]int) (dirname string, cleanup func()) {
+	t.Helper()
+
+	tempDir, err := os.MkdirTemp("", "walktest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for k, n := range files {
+		for j := 1; j <= n; j++ {
+			fname := fmt.Sprintf("file%d%s", j, k)
+			fpath := filepath.Join(tempDir, fname)
+			if err := os.WriteFile(fpath, []byte("test"), 0o644); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	return tempDir, func() { os.RemoveAll(tempDir) }
 }
